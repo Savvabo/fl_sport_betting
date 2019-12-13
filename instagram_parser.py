71fc26f4d4e530413b17206ed611e4a6ba61662b
Kirill-Resh <kiril_r@dataforest.ai>
2019-12-13 20:13:20 +0200

    added proxy logic to isntagram

--- instagram_parser.py
+++ instagram_parser.py
@@ -7,7 +7,8 @@ import logging
 from proxy_helper import ProxyHelper
 import random
 import traceback
-
+import requests
+import time
 logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
                     level=logging.DEBUG)
 logging.getLogger("requests").setLevel(logging.WARNING)
@@ -23,38 +24,115 @@ class InstagramParser:
         self.agent = WebAgentAccount(self.config['instagram']['LOGIN'])
         self.agent.auth(self.config['instagram']['PASSWORD'])
         self.mdb = MDB()
-        self.proxy_list = ProxyHelper().load_proxies_list()
+        self.proxy_list = self.get_proxies()
+        self.use_proxy = False
+
+    @staticmethod
+    def chunkify(l, n):
+        for i in range(0, len(l), n):
+            yield l[i:i + n]
+
+    def check_proxy(self, proxy):
+        try:
+            t1 = time.time()
+            requests.get('https://www.instagram.com', proxies={
+                "http": proxy,
+                "https": proxy,
+            }, timeout=5)
+            request_time = time.time() - t1
+            return {'address': proxy, 'is_valid': True, 'request_time': request_time}
+
+            # return {'address': proxy, 'is_valid': True}
+        except:
+            return {'address': proxy, 'is_valid': False}
+
+    def to_sort_def(self, item):
+        if len(item[1]['insta_request_times']) >= 10:
+            return sum(item[1]['insta_request_times']) /len(item[1]['insta_request_times'])
+        else:
+            return item[1]['check_request_time']
+
+    def sort_proxies(self, proxies_list=None):
+        chosen_proxies_list = proxies_list if proxies_list else self.proxy_list
+        sorted_proxies = {k: v for k, v in sorted(chosen_proxies_list.items(), key=self.to_sort_def)}
+        return sorted_proxies
+
+    def get_proxies(self):
+        raw_proxies = ProxyHelper().load_proxies_list()[:500]
+        chunks = list(self.chunkify(raw_proxies, 500))
+        valid_proxies = {}
+        for n, chunk in enumerate(chunks):
+            logging.info('checking {}/{} proxy batch'.format(n+1, len(chunks)))
+            proxy_pool = ThreadPool(50)
+            checked_proxies = list(proxy_pool.map(self.check_proxy, chunk))
+            proxy_pool.close()
+            valid_batch_proxies = {proxy['address']: {'check_request_time': proxy['request_time'],
+                                                      'error_count': 0,
+                                                      'insta_request_times': []} for proxy in checked_proxies if proxy['is_valid']}
+            valid_proxies.update(valid_batch_proxies)
+        valid_proxies = self.sort_proxies(valid_proxies)
+        logging.info('found {} valid proxies'.format(len(valid_proxies)))
+        return valid_proxies
 
     def mark_proxy_as_failed(self, proxy):
         try:
-            self.proxy_list.remove(proxy)
+            self.proxy_list[proxy]['error_count'] += 1
         except ValueError:
             pass
+        if self.proxy_list[proxy]['error_count'] == 5:
+            try:
+                self.proxy_list.pop(proxy)
+            except KeyError:
+                pass
         if len(self.proxy_list) == 0:
             logging.info('proxy list is empty, getting new proxies')
-            self.proxy_list = ProxyHelper().load_proxies_list()
+            self.proxy_list = self.get_proxies()
+
+    def get_settings(self):
+        if self.use_proxy and random.randint(1, 100) != 50:
+            self.sort_proxies()
+            chosen_proxy = random.choice(list(self.proxy_list.keys())[:len(self.proxy_list)//4])
+        else:
+            chosen_proxy = None
+            if self.use_proxy:
+                self.use_proxy = False
+                logging.info('stop to use proxies')
+        settings = {
+            "proxies": {
+                "http": chosen_proxy,
+                "https": chosen_proxy,
+            },'timeout': 60}
+        return chosen_proxy, settings
 
     def insta_request(self, pointer=None, data=None):
-        settings = {}
-        chosen_proxy = None
+        chosen_proxy, settings = self.get_settings()
+        posts = None
         while True:
             try:
                 if data:
+                    logging.info('start to make request')
                     self.anon_agent.update(data, settings=settings)
-                    return
+                    logging.info('end requests make')
+                    break
                 else:
-                    posts, pointer = self.anon_agent.get_media(self.resource, pointer=pointer, settings=settings)
-                    return posts, pointer
+                    logging.info('start to make request')
+                    t1 = time.time()
+                    posts, pointer = self.anon_agent.get_media(self.resource, pointer=pointer, settings=settings, delay=1)
+                    request_time = time.time() - t1
+                    if chosen_proxy:
+                        self.proxy_list[chosen_proxy]['insta_request_times'].append(request_time)
+                    logging.info('end requests make')
+                    break
             except (InternetException, UnexpectedResponse):
+                logging.info('removing proxy')
                 if chosen_proxy:
                     self.mark_proxy_as_failed(chosen_proxy)
-                chosen_proxy = random.choice(self.proxy_list)
-                settings = {
-                    "proxies": {
-                        "http": chosen_proxy,
-                        "https": chosen_proxy,
-                    },
-                    'timeout': 15}
+                else:
+                    self.use_proxy = True
+                    logging.info('starting to use proxies')
+                chosen_proxy, settings = self.get_settings()
+        if posts:
+            return posts, pointer
 
     def get_new_posts(self):
         stored_posts = self.get_parsed_posts()
@@ -66,7 +144,7 @@ class InstagramParser:
         while posts_count > posts_scraped:
             try:
                 posts, pointer = self.insta_request(pointer=pointer)
-                posts_scraped += 12
+                posts_scraped += len(posts)
                 logging.info(f'scraper {posts_scraped} posts')
                 for post in posts:
                     if post.__str__() not in stored_posts:
